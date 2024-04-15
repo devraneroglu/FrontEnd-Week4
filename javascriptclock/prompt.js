272b1b1f3a11974df39b4ff14d46a599c569bd90
Devran EROĞLU <devraneroglu@gmail.com>
2024-04-16 01:04:15 +0300

    prompt_example

---
+++ javascriptclock/prompt.js
@@ -0,0 +1,14 @@
+let userName = prompt("İsim giriniz : ")
+
+// document.getElementById("myName").textContent = userName;
+document.querySelector("#myName").textContent = userName;
+
+var currentDate = new Date();
+var currentHour = currentDate.getHours();
+var currentMinutes = currentDate.getMinutes();
+var currentDay = currentDate.getDay();
+
+var daysOfWeek = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
+
+document.getElementById("myClock").textContent = 
+ `${currentHour} : ${currentMinutes}   ${daysOfWeek[currentDay]} ` ;
